a6dd5b356c65a3742c7442b189254adf1c1a3ad5
waleedmalik7 <waleedmalik027@gmail.com>
2022-08-01 15:15:15 -0400

    optimal solution

--- src/substring.java
+++ src/substring.java
@@ -3,25 +3,28 @@ import java.util.HashSet;
 public class substring {
     public int lengthOfLongestSubstring(String s) {
 
+        int first = 0;
+        int second = 0;
+
+        HashSet<Character> mySet = new HashSet<>();
+
         int total = 0; 
         int count = 0;
         
-        for(int i = 0; i < s.length(); i++){
-            HashSet<Character> mySet = new HashSet<>();
-            int index = i;
 
-            while(!mySet.contains(s.charAt(index))){
+        while(first < s.length()){
+            if(!mySet.contains(s.charAt(first))){
+                mySet.add(s.charAt(first));
                 count++;
-                mySet.add(s.charAt(index));
-                if(index < s.length()-1){
-                    index++;
-                }
+                first++;
+                total = Math.max(total, count);
+            }else{
+                mySet.remove(s.charAt(second));
+                second++;
+                count--;
             }
-
-            total = count >= total ? count : total; //update counter
-            count =0;
         }
-        
+
         return total;
         
     }
